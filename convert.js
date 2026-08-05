1c4b08c48a9faf9ea74dc9f446c8bc5ecc766aea
payent <thomaspayen7898989@gmail.com>
2026-08-06 01:26:44 +0200

    Ajout messages 365 jours et projet complet

---
+++ convert.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const messages = require("./messages.js");
+
+fs.writeFileSync(
+  "messages.json",
+  JSON.stringify(messages, null, 2),
+  "utf8"
+);
+
+console.log("messages.json créé !");
