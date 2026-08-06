81dbdd2db94f11cbf4a2d71f9f0640c7a51aba62
payent <thomaspayen7898989@gmail.com>
2026-08-06 13:24:55 +0200

    Amélioration service worker PWA

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,44 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "myway-cache-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./manifest.json",
+    "./icon.png"
+];
+
+
+self.addEventListener("install", event => {
+
     console.log("MyWay installé ❤️");
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+
 });
 
-self.addEventListener("activate", () => {
+
+self.addEventListener("activate", event => {
+
     console.log("MyWay actif ❤️");
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
 });
